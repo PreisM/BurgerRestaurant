81580a5a7f485d5ba67df1c2f218abac6437df92
PreisM <59081755+PreisM@users.noreply.github.com>
2020-01-07 18:28:47 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,9 @@
 $(document).ready(function() {
-
     $('.navbar-toggler').click(function() {
-
         $('.navbar-toggler').toggleClass('change')
-
-
     })
-
     $(window).scroll(function() {
-
         let position = $(this).scrollTop();
-
         if (position >= 718) {
             $('.navbar').addClass('navbar-background');
             $('.navbar').addClass('fixed-top');
@@ -18,45 +11,28 @@ $(document).ready(function() {
             $('.navbar').removeClass('navbar-background');
             $('.navbar').removeClass('fixed-top');
         }
-
     })
 
     $('.nav-item a, .header-link, #back-to-top').click(function(link) {
-
         link.preventDefault();
         let target = $(this).attr('href');
-
         $('html, body').animate({
             scrollTop: $(target).stop().offset().top - 25
         }, 2500);
-
     })
 
     $(window).scroll(function() {
-
         let position = $(this).scrollTop();
-
         if (position >= 718) {
             $('#back-to-top').addClass('scrollTop');
-
         } else {
             $('#back-to-top').removeClass('scrollTop');
         }
-
     })
-
-
-
-
-
-
-
-
-
-
+    
     $("#header, .info").ripples({
-        dropRadius: 10,
-        perturbance: 0.6,
+        dropRadius: 1,
+        perturbance: 0.2,
     });
 
     $('.parent-container').magnificPopup({
